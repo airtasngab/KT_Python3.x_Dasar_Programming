ef3607bbb104d31c81ee305b82c797999bdbf130
airtasngab <airtasngab@gmail.com>
2024-12-06 05:01:25 +0000

    add recap folder & file

---
+++ !RECAP/satu.py
@@ -0,0 +1,37 @@
+'''
+00
+print -> print()
+
+02
+*install requirements
+
+03
+-single & multi-line comments
+-python -> interpreter, tapi bisa dicompile menggunakan bytecode:
+di terminal dgn command: python -m py_compile main.py (file-name.py)
+-compile menjalankan program lebih cepat dari pada cara interpreter, akan terasa signifikan apabila baris program semakin banyak.
+
+04
+-variabel -> tempat menyimpan nilai
+penamaan var. -> tdk boleh diawali angka & tdk boleh memiliki spasi, untuk cara penamaan lainnya dpt berubah2 peraturannya berdasarkan versi pythonnya. penamaan normal gunakan camell case/ gris bawah.
+
+05
+tipe data -> jenis suatu data, terdiri dari:
+-integer as int: bil. bulat
+-float as float: bil. pecahan .x
+-string as str: kumpulan char/ teks
+-bool as bool: bil.logika
+-tipe data khusus 
+--complex as complex: bil. imajiner?
+--tipe data C, dari library ctypes import seperti c_double, c_char, c_long, dll.
+
+06
+-casting tipe data -> mengkonversi tipe data:
+-konver input user: 
+
+07
+
+
+'''
+
+print('X = ?')
